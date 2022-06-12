1b2fa8c6e75daad9be6defe8b0752e8d1fcec1f5
s-oo <d-aoo@naver.com>
2022-06-13 00:53:16 +0900

    Learn about Basics of Java
    print, println

---
+++ src/day1/Ex1.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");//출력: 저장(Ctrl+S)후 재생 or Ctrl+F11
+		System.out.print("Hi");//syso + Ctrl+Space
+
+	}
+
+}
+
+/*
+print: 줄바꿈X
+println: 줄바꿈
+
+주석처리: //
+*/
